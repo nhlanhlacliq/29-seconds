580213b19a6457be8ce2c819bb4ce8a7eb19694c
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-03-09 18:39:00 +0200

    clean up difficulty class importing

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from website import create_app
+from website.difficulty import Difficulty as diff
 from flask import Flask, render_template, request, url_for, Response
 
 
 app = create_app()
 
 
-
 @app.route('/difficulty-mode/<int:difficulty_mode>')
-def dynamic_difficulty(difficulty_mode):
-  return f"Flask: Call Difficulty({difficulty_mode})"
+def difficulty_mode(difficulty_mode):
+    # dm = Difficulty mode
+    dm = diff(difficulty_mode)
+    level = dm.get_difficulty()
+    time = dm.get_time_limit()
+    
+    return f"Difficulty level: {level}. \nTime limit: {time}"
 
 if __name__ == "__main__":
-  app.run(host="127.0.0.1", port = 8080, debug=True)
+    app.run(host="127.0.0.1", port = 8080, debug=True)
