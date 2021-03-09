28b04f988c8239f57a90ec33826cbbe0849f96d2
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-03-09 18:18:03 +0200

    code clean up. removed unnecesarry code

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
 from website import create_app
 from flask import Flask, render_template, request, url_for, Response
-from app import Game
+
 
 app = create_app()
 
-difficulty_mode = 0
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method == 'POST':
-      return f'hello. difficulty mode is {difficulty_mode}'
-    else:
-      return render_template("difficulty_select.html")
 
-@app.route('/dynamic_difficulty/')
-def dynamic_difficulty():
-  difficulty_mode = 1
-  return f"Flask: Call Difficulty({difficulty_mode})"
 
-@app.route('/dance_difficulty/')
-def dance_difficulty():
-  difficulty_mode = 2
-  return f"Flask: Call Difficulty({difficulty_mode})"
-
-@app.route('/custom_difficulty/')
-def custom_difficulty():
-  difficulty_mode = 3
+@app.route('/difficulty-mode/<int:difficulty_mode>')
+def dynamic_difficulty(difficulty_mode):
   return f"Flask: Call Difficulty({difficulty_mode})"
 
 if __name__ == "__main__":
