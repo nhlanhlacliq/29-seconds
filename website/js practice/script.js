20ffb5ba70c7d6c8290fa7e12af63799b6d5a2d1
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-03-29 14:24:21 +0200

    hm

--- website/js practice/script.js
+++ website/js practice/script.js
@@ -1,10 +1,20 @@
-var scoops = 5;
+// var scoops = 5;
 
-while (scoops > 0) {
-  console.log("SCOOP!<br>");
-  scoops -= 1;
+// while (scoops > 0) {
+//   console.log("SCOOP!<br>");
+//   scoops -= 1;
 
-  if (scoops < 3) {
-    alert("Ice cream running low");
-  }
+//   if (scoops < 3) {
+//     alert("Ice cream running low");
+//   }
+// }
+
+var name = "Joe";
+var i = 0;
+
+while (i < 2) {
+  document.write("Happy Birthday to you.<br>");
+  i++;
 }
+document.write("Happy Birthday dear " + name + ",<br>");
+document.write("Happy Birthday to you.<br>");
