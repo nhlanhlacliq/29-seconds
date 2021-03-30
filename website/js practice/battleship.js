d7f7a3da6918460b790fd723b0f6b19840e8312f
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-03-30 10:18:35 +0200

    practice

--- website/js practice/battleship.js
+++ website/js practice/battleship.js
@@ -5,3 +5,20 @@ var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false;
+
+while (!isSunk) {
+  guess = prompt("Ready, aim, fire! (enter a number 0-6):");
+  if (0 < guess < 6) {
+    alert("Invalid guess");
+  } else {
+    guesses++;
+    if ((guess == location1) | (guess == location2) | (guess == location3)) {
+      hits++;
+      if (hits == 3) {
+        isSunk = true;
+        alert("You sunk my ship!");
+      }
+    }
+  }
+}
+alert("User stats");
