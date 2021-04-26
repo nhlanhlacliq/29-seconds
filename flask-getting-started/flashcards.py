092383935a8eec72814749960eebdedb513c9750
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-04-26 09:52:49 +0200

    add page counts

--- flask-getting-started/flashcards.py
+++ flask-getting-started/flashcards.py
@@ -3,9 +3,18 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+page_views = 0
+
 @app.route('/')
 def welcome():
     return "Welcome to my Flash Cards application"
+
+# Add a page that shows how many times it has been viewed
+@app.route('/views')
+def views():
+    global page_views
+    page_views += 1
+    return f"This page has been viewed {page_views} times"
 
 @app.route('/date')
 def date():
