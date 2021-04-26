f74a975e00d954012f77b4149c589ac89bb1338a
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-04-26 14:59:16 +0200

    added card list to homepage

--- flask-getting-started/flashcards.py
+++ flask-getting-started/flashcards.py
@@ -7,12 +7,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return render_template("welcome.html", message="Here's a message from the view.")
+    return render_template("welcome.html", 
+                            cards = db)
 
 @app.route('/card/<int:index>')
 def card_view(index):
     try:
         card = db[index]
-        return render_template("card_view.html", card=card, index=index, max_index=len(db)-1)
+        return render_template("card_view.html", 
+                                card=card, 
+                                index=index, 
+                                max_index=len(db)-1)
     except IndexError:
         abort(404)
