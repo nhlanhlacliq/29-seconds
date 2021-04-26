a1f810463eb0377b157aa794c99ceba77657e965
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-04-26 09:54:50 +0200

    refact

--- flask-getting-started/flashcards.py
+++ flask-getting-started/flashcards.py
@@ -3,19 +3,20 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-page_views = 0
 
 @app.route('/')
 def welcome():
     return "Welcome to my Flash Cards application"
 
+@app.route('/date')
+def date():
+    return f"This page was served at {(str(datetime.now()))[:-7]}."
+
 # Add a page that shows how many times it has been viewed
+page_views = 0
+
 @app.route('/views')
 def views():
     global page_views
     page_views += 1
     return f"This page has been viewed {page_views} times"
-
-@app.route('/date')
-def date():
-    return f"This page was served at {(str(datetime.now()))[:-7]}."
