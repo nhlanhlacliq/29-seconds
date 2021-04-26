565561918b8d9b38d5199d6f5a2d5a604463a0b4
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-04-26 16:05:42 +0200

    added post on py

--- flask-getting-started/flashcards.py
+++ flask-getting-started/flashcards.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort, url_for, jsonify
+from flask import Flask, render_template, abort, url_for, jsonify, request
 from model import db 
 
 app = Flask(__name__)
@@ -33,8 +33,14 @@ def api_card_detail(index):
     except IndexError:
         abort(404)
 
-@app.route('/add_card/')
+@app.route('/add_card/', methods=['GET','POST'])
 def add_card():
-    return render_template("add_card.html")
+    if request.method == 'POST':
+        # Process user database
+        card = {"question":request.form['question'],
+        "answer":request.form['answer']}
+        db.append(card)
+    else:
+        return render_template("add_card.html")
 
 
