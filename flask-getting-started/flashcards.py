27750b5a71b896e8050e99cdbd9203c6985ceb12
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-04-26 09:47:03 +0200

    added datetime

--- flask-getting-started/flashcards.py
+++ flask-getting-started/flashcards.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
     return "Welcome to my Flash Cards application"
+
+@app.route('/date')
+def date():
+    return f"This page was served at {(str(datetime.now()))[:-7]}."
